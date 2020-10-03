154d6558eca641b39fb7156b1f6c857d9c636e7d
RavinduSM <70681852+RavinduSM@users.noreply.github.com>
2020-10-03 21:03:12 +0530

    updated

---
+++ Website/adduser.php
@@ -0,0 +1,32 @@
+
+
+<?php
+
+    $fname=$_POST["txt_f_name"];
+    $lname=$_POST["txt_l_name"];
+    $contact=$_POST["txt_tele"];
+    $email=$_POST["txt_email"];
+    $pw=$_POST["txtpassword"];
+	$address=$_POST["txt_address"];
+	$country=$_POST["txt_country"];
+	$zip=$_POST["txt_zipcode"];
+	
+    //connecting to database
+    $con=mysqli_connect("localhost","root","","nse_e_com");
+    if(!$con){
+        die("Error occured in db connection, Please try again");
+    }
+
+    //inserting values to sql
+    $sql="INSERT INTO `customer`(`C_Email`, `C_password`, `C_fname`, `C_lname`, `C_Address`, `C_Country`, `C_ZipCode`, `C_Phone`)VALUES ('".$email."', '".$pw."' , '".$fname."' , '".$lname."', '".$address."','".$country."', '".$zip."', '".$contact."')";
+    
+    //validation and redirection
+    if(mysqli_query($con,$sql)){
+        header('Location:login.php');
+    }
+
+    //closing database connection
+    mysqli_close($con);
+    
+
+?>
---
+++ Website/details.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+	<link rel="stylesheet" type="text/css" href="css/details.css">
+</head>
+
+<body>
+	
+	<form id="addtocart" name="addtocart" action="addtocart.php" method="post">
+	<div class="content-wrapper">
+		 <?php
+		
+		 $con = mysqli_connect("localhost","root","","nse_e_com");
+		if(!$con)
+		{	
+			die("Cannot connect to DB server");	
+		}
+				  
+				 $sql ="SELECT * FROM `product` WHERE `product`.`P_ID` = ".$_GET["pid"];	
+				  
+					
+		$result = mysqli_query($con,$sql);
+			
+		if(mysqli_num_rows($result)> 0)
+	{
+			while($row = mysqli_fetch_assoc($result))
+			{
+			
+	?>
+    <img src="<?php echo $row['P_filepath']?>" width="400" height="400" />
+		
+    <div class="content">
+        <h1 class="name"><?php echo $row["P_Name"]; ?></h1>
+		<span class="price">
+            &dollar;<?php echo $row["P_Price"]; ?>
+        </span>
+        <br>
+            <input type="number" name="txt_quantity" value="1" min="1" max="" placeholder="Quantity" required><br>
+            <input type="hidden" name="txt_productid" value="<?php echo $row['P_ID']?> ">
+			<input type="hidden" name="txt_productname" value="<?php echo $row['P_Name']?> ">
+			<input type="hidden" name="txt_productprice" value="<?php echo $row['P_Price']?> ">
+            <input type="submit" value="Add To Cart">
+      
+		<h3>Description</h3>
+        <div class="description">
+           	<?php echo $row["P_Description"]; ?>
+        </div>
+		 </div>
+		
+	<?php
+			}
+	}
+	
+	?>
+    
+</div>
+		 </form>
+
+</body>
+</html>
