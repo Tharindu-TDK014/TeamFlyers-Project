e3258a08dc492358b4e860f3eedd9fcf2c0655ea
Tharindu-TDK014 <tharindudk7714@gamil.com>
2020-10-05 19:34:14 +0530

    Remove Cart

--- Website/dbcon.php
+++
@@ -1,8 +0,0 @@
-
-<?php	$con = mysqli_connect("localhost","root","","database name ");
-    if (mysqli_connect_errno()){
- echo "Failed to connect to MySQL: " . mysqli_connect_error();
- die();
- }?>
-	
-	
